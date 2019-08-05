8844d2190dce5c96f2c73d3d832b42c259c2d577
Ravi Teja <teja.pieboy@gmail.com>
2019-08-05 13:32:23 +0000

    updates

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,19 +1,27 @@
 var webpack = require('webpack');
-new webpack.optimize.CommonsChunkPlugin('common.js'),
-new webpack.optimize.DedupePlugin(),
-new webpack.optimize.UglifyJsPlugin(),
+// new webpack.optimize.CommonsChunkPlugin('common.js'),
+// new webpack.optimize.DedupePlugin(),
+// new webpack.optimize.UglifyJsPlugin(),
 new webpack.optimize.AggressiveMergingPlugin()
 
 
 const path = require('path');
 
 const config = {
+  mode: 'production',
   entry: [
     './index.js',
   ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    splitChunks: {
+        chunks: "all"
+    },
+    minimize: true //Update this to true or false
+
   },
   module: {
     rules: [
